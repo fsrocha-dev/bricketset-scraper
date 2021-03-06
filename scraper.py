540ba4f9f20c34656350b9f97466433a296f63ad
fsrocha-dev <contato.frankrocha@gmail.com>
2021-03-06 18:32:11 -0300

    Pega texto e o autor

--- scraper.py
+++ scraper.py
@@ -1,6 +1,15 @@
 import scrapy
 
-class BrickSetSpider(scrapy.Spider):
-    name = "bricketset_spider"
-    start_urls = ['http://brickset.com/sets/year-2020']
+class QuotesSpider(scrapy.Spider):
+    name = "brickset"
+    start_urls = ['http://quotes.toscrape.com']
     
+    def parse(self, response):
+        SET_SELECTOR = '.quote'
+        for quote in response.css(SET_SELECTOR):
+            TEXT_SELECTOR = 'span ::text'
+            AUTHOR_SELECTOR = 'span small ::text'
+            yield {
+                'text': quote.css(TEXT_SELECTOR).extract_first(),
+                'Author': quote.css(AUTHOR_SELECTOR).extract_first(),
+            }
